c557e907a41f242cc13dae27dd9175da3f15a0b2
Vinicio Almeida <121837497+vinicioalmeida@users.noreply.github.com>
2024-01-15 18:26:24 -0300

    binomial

--- derivativosempy04.py
+++ derivativosempy04.py
@@ -30,54 +30,20 @@ def binomial_tree_option_pricing(S, K, T, r, sigma, n, option_type='call'):
     return option_price
 
 # Parâmetros
-S0 = 38.17      # Preço spot inicial da ação
+S = 38.17      # Preço spot inicial da ação
 K = 40.01       # Preço de exercício
 T = 22/252      # Vencimento em anos
 r = 0.115685    # Taxa de juros livre de risco (anual)
 sigma = 0.25    # Volatilidade anual
 
 # Número de passos
-n = int(2)
+n = int(5)
 
 # Cálculo do preço da opção de compra (call)
-call_price = binomial_tree_option_pricing(S0, K, T, r, sigma, n, option_type='call')
+call_price = binomial_tree_option_pricing(S, K, T, r, sigma, n, option_type='call')
 
 # Cálculo do preço da opção de venda (put)
-put_price = binomial_tree_option_pricing(S0, K, T, r, sigma, n, option_type='put')
+put_price = binomial_tree_option_pricing(S, K, T, r, sigma, n, option_type='put')
 
 print(f"O preço da opção de compra (call) com {n} passos é: {call_price:.2f}")
 print(f"O preço da opção de venda (put) com {n} passos é: {put_price:.2f}")
-
-
-# Black-Scholes-Merton - Derivativos em Py #02
-
-import math
-
-def black_scholes_call_put(S, K, T, r, sigma, option_type):
-    d1 = (math.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * math.sqrt(T))
-    d2 = d1 - sigma * math.sqrt(T)
-    
-    if option_type == 'call':
-        option_price = S * norm_cdf(d1) - K * math.exp(-r * T) * norm_cdf(d2)
-    elif option_type == 'put':
-        option_price = K * math.exp(-r * T) * norm_cdf(-d2) - S * norm_cdf(-d1)
-    else:
-        raise ValueError("Tipo de opção inválido. Use 'call' ou 'put'.")
-    
-    return option_price
-
-def norm_cdf(x):
-    return (1.0 + math.erf(x / math.sqrt(2.0))) / 2.0
-
-# Um exemplo
-S = 38.17
-K = 40.01  
-T = 22/252
-r = 0.115685 
-sigma = 0.25 
-
-precoCall = black_scholes_call_put(S, K, T, r, sigma, 'call')
-precoPut = black_scholes_call_put(S, K, T, r, sigma, 'put')
-
-print(f"Preço da call: {precoCall:.2f}")
-print(f"Preço da put: {precoPut:.2f}")
